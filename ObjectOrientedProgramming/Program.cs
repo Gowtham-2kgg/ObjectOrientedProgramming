2c1ad7198a869d40c50f7f504997de77c610e99d
Gowtham-2kgg <bgowtham324@gmail.com>
2023-04-08 15:51:47 +0530

    Project scope added

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+//Inheritance
+//Polymorphism
+//Abstraction
+//Encapsulation
+//access modifiers
+//properties of attributes
+//Constructors and Base
+//Partial, Sealed, Static, Abstract classes,Interfaces ,and their diffrences
+//overriding(virtual, abstract) and overloading
+/*Questions{
+can a supercalss object refer subclass and vice versa
+can private, protected and internal methods can be overloaded or overrided
+can access modifiers used in overloading
+}*/ 
