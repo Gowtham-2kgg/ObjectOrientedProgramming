5135e10d2cfcf08b69a3c1224ef63d85985aae28
Gowtham-2kgg <bgowtham324@gmail.com>
2023-04-08 18:07:57 +0530

    oops over

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine("Hello, World!");
 //Partial, Sealed, Static, Abstract classes,Interfaces ,and their diffrences
 //overriding(virtual, abstract) and overloading
 /*Questions{
-can a supercalss object refer subclass and vice versa
+can a supercalss object refer subclass and vice versa(top to down possible but not down to top- down means sub and up means supoer)
 can private, protected and internal methods can be overloaded or overrided
 can access modifiers used in overloading
 }*/ 
